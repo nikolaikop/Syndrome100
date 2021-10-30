cf250ee900fcbadc73a116d8f52ee06715ca281f
Nikolai Pustovoit <nikolaikop@gmail.com>
2021-10-30 12:32:13 -0600

    one more test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void currentMaxBelowIncome() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {3, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
